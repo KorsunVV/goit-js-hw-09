3e554b70c8cd4ee7df4ee88ed1ed5929fc0414a8
KorsunVV <korsunvv.work@gmail.com>
2022-05-03 21:41:03 +0300

    version2NEW
    Полностью по новому первое задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,28 @@
+const body = document.querySelector('body');
+let intervalId = null;
+
+body.addEventListener('click', onBtnClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-let intervalId = 0;
+function startInterval() {
+  body.style.backgroundColor = getRandomHexColor();
+}
 
-const start = document.querySelector('[data-start]');
-const stop = document.querySelector('[data-stop]');
-stop.disabled = true;
+function onBtnClick(element) {
+  if (element.target.textContent === 'Start') {
+    element.target.disabled = true;
+    element.target.nextElementSibling.disabled = false;
+    intervalId = setInterval(() => {
+      startInterval();
+    }, 1000);
+  } else {
+    clearInterval(intervalId);
+    element.target.disabled = true;
+    element.target.previousElementSibling.disabled = false;
+    intervalId = null;
+  }
+}
 
-const onStartBtnClick = () => {
-  start.disabled = true;
-  stop.disabled = false;
-  document.body.style.backgroundColor = getRandomHexColor();
-
-  intervalId = setInterval(() => {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-};
-
-const onStopBtnClick = () => {
-  clearInterval(intervalId);
-  stop.disabled = true;
-  start.disabled = false;
-};
-
-start.addEventListener('click', onStartBtnClick);
-stop.addEventListener('click', onStopBtnClick);
