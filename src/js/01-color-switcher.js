748c7fdc9b6cabadceded9c75010af9022ea5f3b
KorsunVV <korsunvv.work@gmail.com>
2022-05-03 19:13:27 +0300

    debag
    Исправленая кнопка старт

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,6 +3,7 @@ function getRandomHexColor() {
 }
 
 let intervalId = 0;
+
 const start = document.querySelector('[data-start]');
 const stop = document.querySelector('[data-stop]');
 stop.disabled = true;
@@ -11,6 +12,7 @@ const onStartBtnClick = () => {
   start.disabled = true;
   stop.disabled = false;
   document.body.style.backgroundColor = getRandomHexColor();
+
   intervalId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
@@ -19,9 +21,8 @@ const onStartBtnClick = () => {
 const onStopBtnClick = () => {
   clearInterval(intervalId);
   stop.disabled = true;
-  star.disabled = false;
+  start.disabled = false;
 };
-
 
 start.addEventListener('click', onStartBtnClick);
 stop.addEventListener('click', onStopBtnClick);
